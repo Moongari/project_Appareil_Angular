0e8fb02f6652a1d20dc4f6288e7b88e0ae4315d1
SalamMoon <moungari.moustafa@gmail.com>
2021-01-29 18:21:20 +0100

    ajout d'un objet appareil image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
+  import {Component, Input, OnInit} from '@angular/core';
+  import { Observable } from 'rxjs/observable';
+  // tslint:disable-next-line:import-blacklist
+  import 'rxjs/Rx';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-constructor() {
-}
-  title = 'MOON PROJET ANGULAR';
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent implements OnInit{
+
+    secondes : number;
+  constructor() {
+  }
+    title = 'MOON PROJET ANGULAR';
+
+    ngOnInit(): void {
+
+      const counter = Observable.interval(1000);
+
+      counter.subscribe( (value :number )=>
+                            {
+                              this.secondes= value
+                            },
+                            (error:any) =>
+                            {
+                                console.log('error rencontré')
+                            },
+                    () =>{
+                          console.log('Complete')
+                        });
 
 
-}
+    }
+
+
+  }
